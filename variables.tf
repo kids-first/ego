0bdf6d6a885b615d48d8f093fe6a5d76765d5b0b
Jonathan Ganesh <jganesh@olm-jganesh.ad.oicr.on.ca>
2018-04-02 13:24:04 -0400

    Set variables to point to vault and RDS

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "service_name" {
 variable "environment" {
 }
 variable "application" {
-  default = "not_set"
+  default = "egoserverservice-api"
 }
 variable "organization" {
   default="kf"
@@ -17,15 +17,17 @@ variable "bucket" {}
 variable "owner" {}
 variable "organzation" {}
 variable "image" {
+  default="538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice"
 }
 variable "task_role_arn" {
 }
-variable "vault_url" {}
+variable "vault_url" {
+}
 variable "pg_host" {}
 variable "db_secret_path" {
   default=""
 }
 variable "pg_db_name" {
-  default = ""
+  default = "ego"
 }
 variable "vault_role" {}
