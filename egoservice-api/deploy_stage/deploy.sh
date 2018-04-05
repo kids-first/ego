d7439f13b15dc31dc2fb2dfa698c898500d28fd0
Jonathan Ganesh <jganesh@olm-jganesh.ad.oicr.on.ca>
2018-04-05 15:54:39 -0400

    fix syntax issue for Terraform

--- egoservice-api/deploy_stage/deploy.sh
+++ egoservice-api/deploy_stage/deploy.sh
@@ -7,7 +7,7 @@ if [ $1 = "dev" ]; then
   echo "Setting up backend"
   echo 'key        = "dev/kf-dev-pi-egoserverservice-us-east-1-RSF"' >> dev.conf
   terraform init -backend=true -backend-config=dev.conf
-   validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice:latest' \
+  terraform validate -var 'image=538745987955.dkr.ecr.us-east-1.amazonaws.com/kf-api-egoserverservice:latest' \
    -var 'task_role_arn="arn:aws:iam::538745987955:role/kfEgoserviceApiRole-dev"' -var 'application=egoserverservice-api' \
    -var 'service_name="kf-api-egoserverservice"' -var 'owner="jenkins"' -var-file=dev.tfvar \
    -var 'vault_role="kf_egoserverservice_api_role"' -var 'pg_host="ego-dev-db.c3siovbugjym.us-east-1.rds.amazonaws.com"' \
